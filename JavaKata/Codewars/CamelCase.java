c591ea9c3c2bdb095ef999aeafa951b7cfbb1d30
jgolden5 <jonathan.golden.gurr@gmail.com>
2026-04-02 06:40:35 -0700

    solved CamelCase kata in Java

--- JavaKata/Codewars/CamelCase.java
+++ JavaKata/Codewars/CamelCase.java
@@ -1,7 +1,22 @@
 package Codewars;
 
 class CamelCase {
+  
   public static String camelCase(String input) {
-    return "";
+    String res = "";
+    for(int i = 0; i < input.length(); i++) {
+      char currentChar = input.charAt(i);
+      if(isUppercase(currentChar)) {
+        res += " " + currentChar;
+      } else {
+        res += currentChar;
+      }
+    }
+    return res;
   }
+  
+  public static boolean isUppercase(char c) {
+    return (int) c >= 65 && c <= 90;
+  }
+  
 }
--- JavaKata/Codewars/Main.java
+++ JavaKata/Codewars/Main.java
@@ -4,8 +4,10 @@ public class Main {
   public static void main(String[] args) {
     String t1 = CamelCase.camelCase("testInput");
     if(t1.equals("test Input")) {
+      System.out.println(t1 + " = test Input");
       System.out.println("Test passed! Good job :)");
     } else {
+      System.out.println("test Input != " + t1);
       System.out.println("Test failed... :(");
     }
   }
