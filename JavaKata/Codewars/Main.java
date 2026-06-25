a17e6515017766c8613bc5b3937f9437fb42cce3
jgolden5 <jonathan.golden.gurr@gmail.com>
2026-06-25 13:56:52 -0700

    solved find_a_b.py codewars

--- JavaKata/Codewars/Main.java
+++ JavaKata/Codewars/Main.java
@@ -5,8 +5,6 @@ import java.util.regex.Matcher;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println(Order.order("is2 Thi1s T4est 3a"));
-    System.out.println(Order.order("I1 lo2ve i4tems 5on m3y wish7 lis6t"));
-    System.out.println(Order.order(""));
+    System.out.println("esooo");
   }
 }
